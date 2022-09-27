954277007ae3d072c271a6f726ba18c9017a4fbb
NuggoDEV <NuggoDEV@gmail.com>
2022-09-27 17:28:24 +0930

    Mod UI Stuff

--- src/ModUI/QolourSwitcherUI.cpp
+++ src/ModUI/QolourSwitcherUI.cpp
@@ -6,19 +6,67 @@ using namespace QolourSwitcher;
 
 #include "questui/shared/BeatSaberUI.hpp"
 #include "questui/shared/QuestUI.hpp"
+using namespace QuestUI;
 
 #include "UnityEngine/GameObject.hpp"
 using namespace UnityEngine;
+using namespace GlobalNamespace;
+
+#include "GlobalNamespace/PlayerDataModel.hpp"
+#include "GlobalNamespace/PlayerData.hpp"
+#include "GlobalNamespace/ColorScheme.hpp"
+#include "GlobalNamespace/ColorSchemesSettings.hpp"
+
 
 DEFINE_TYPE(QolourSwitcher, QolourSwitcherUI);
 
 
-
 GameObject *leftScreen;
+bool screenOn = false;
 
 void QolourSwitcher::QolourSwitcherUI::DidActivate(bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling)
 {
 
-   leftScreen = QuestUI::BeatSaberUI::CreateFloatingScreen(Vector2(20.0f, 60.0f), Vector3(-1.8f, 1.0f, -2.0f), Vector3(-35.0f, 0.0f, 0.0f), 0.0f, true, false);
+   auto playerDataModel = Object::FindObjectOfType<PlayerDataModel *>();
+   auto playerData = playerDataModel->playerData;
+   auto colourScheme = playerData->colorSchemesSettings->GetColorSchemeForId(playerData->colorSchemesSettings->selectedColorSchemeId);
 
+   if (firstActivation)
+   {
+      GameObject *centreScreen = BeatSaberUI::CreateScrollView(get_transform());
+      leftScreen = QuestUI::BeatSaberUI::CreateFloatingScreen(Vector2(60.0f, 70.0f), Vector3(-2.1f, 1.0f, 2.8f), Vector3(0.0f, -35.0f, 0.0f), 0.0f, true, false);
+
+      // Mod Toggle Switch
+      BeatSaberUI::CreateToggle(leftScreen->get_transform(), "Disable Qolour Switcher", getModConfig().ModToggle.GetValue(), Vector2(0.0f, -5.0f), [](bool value)
+      {
+         getModConfig().ModToggle.SetValue(value);
+      });
+
+
+
+      // Colour Picker Creators
+      BeatSaberUI::CreateColorPicker(centreScreen->get_transform(), "Left Saber Colour", colourScheme->get_saberAColor(), [colourScheme](Color colour)
+      {
+         colourScheme->saberAColor = colour;
+      });
+
+      BeatSaberUI::CreateColorPicker(centreScreen->get_transform(), "Right Saber Colour", colourScheme->get_saberBColor(), [colourScheme](Color colour)
+      {
+         colourScheme->saberBColor = colour;
+      });
+   }
+
+   if (!screenOn)
+   {
+      screenOn = true;
+      leftScreen->SetActive(true);
+   }
+
+
+}
+
+void QolourSwitcher::QolourSwitcherUI::DidDeactivate(bool removedFromHierarchy, bool screenSystemDisabling)
+{
+   screenOn = false;
+   leftScreen->SetActive(false);
 }
