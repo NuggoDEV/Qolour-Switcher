324c5fab7793663d1384cb1637c7a4d6f819f1f1
NuggoDEV <NuggoDEV@gmail.com>
2022-09-27 21:48:31 +0930

    Tidied up config

--- include/QolourSwitcherConfig.hpp
+++ include/QolourSwitcherConfig.hpp
@@ -3,13 +3,9 @@
 #include "config-utils/shared/config-utils.hpp"
 
 DECLARE_CONFIG(ModConfig, 
-
     CONFIG_VALUE(ModToggle, bool, "Disable Qolour Switcher", false);
-
-
+    
     CONFIG_INIT_FUNCTION(
         CONFIG_INIT_VALUE(ModToggle);
     )
-
-
 )
