8a6a5e3d21ea3c72a0669eb88b162ecab0f30bf3
NuggoDEV <NuggoDEV@gmail.com>
2022-10-02 21:23:43 +1030

    Add a bool to update the colours

--- include/QolourSwitcherConfig.hpp
+++ include/QolourSwitcherConfig.hpp
@@ -4,8 +4,10 @@
 
 DECLARE_CONFIG(ModConfig, 
     CONFIG_VALUE(ModToggle, bool, "Disable Qolour Switcher", false);
+    CONFIG_VALUE(ColoursChanged, bool, "", false);
     
     CONFIG_INIT_FUNCTION(
         CONFIG_INIT_VALUE(ModToggle);
+        CONFIG_INIT_VALUE(ColoursChanged);
     )
 )
