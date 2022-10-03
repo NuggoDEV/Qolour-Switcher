e32fa1a09cd2d5a449fc2e7f825fe1aaec349bba
NuggoDEV <NuggoDEV@gmail.com>
2022-10-04 00:18:30 +1030

    Set custom colours to true on level load (I think)

--- src/Hooks/CustomColoursToggle.cpp
+++ src/Hooks/CustomColoursToggle.cpp
@@ -2,6 +2,8 @@
 #include "QolourSwitcherHooks.hpp"
 
 #include "GlobalNamespace/MainMenuViewController.hpp"
+#include "GlobalNamespace/GameCoreSceneSetup.hpp"
+#include "GlobalNamespace/StandardLevelDetailViewController.hpp"
 #include "GlobalNamespace/ColorSchemeColorToggleController.hpp"
 #include "GlobalNamespace/PlayerDataModel.hpp"
 #include "GlobalNamespace/PlayerData.hpp"
@@ -22,4 +24,18 @@ MAKE_AUTO_HOOK_MATCH(MainMenuViewController_DidActivate, &MainMenuViewController
     {
         playerData->colorSchemesSettings->overrideDefaultColors = true;
     }
-}   
+}
+
+MAKE_AUTO_HOOK_MATCH(StandardLevelDetailViewController_DidActivate, &StandardLevelDetailViewController::DidActivate, void, StandardLevelDetailViewController *self, bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling)
+{
+    StandardLevelDetailViewController_DidActivate(self, firstActivation, addedToHierarchy, firstActivation);
+
+    auto playerDataModel = Object::FindObjectOfType<PlayerDataModel *>();
+    auto playerData = playerDataModel->playerData;
+    bool customColours = playerData->colorSchemesSettings->overrideDefaultColors; 
+    
+    if (!customColours)
+    {
+        playerData->colorSchemesSettings->overrideDefaultColors = true;
+    }    
+}
