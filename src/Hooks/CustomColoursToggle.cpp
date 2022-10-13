329df84248838ec3379165980eb1d2f73e3780ee
NuggoDEV <NuggoDEV@gmail.com>
2022-10-13 22:47:41 +1030

    Moved a hook out of here

--- src/Hooks/CustomColoursToggle.cpp
+++ src/Hooks/CustomColoursToggle.cpp
@@ -12,19 +12,6 @@ using namespace GlobalNamespace;
 #include "UnityEngine/Object.hpp"
 using namespace UnityEngine;
 
-MAKE_AUTO_HOOK_MATCH(MainMenuViewController_DidActivate, &MainMenuViewController::DidActivate, void, MainMenuViewController *self, bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling)
-{
-    MainMenuViewController_DidActivate(self, firstActivation, addedToHierarchy, screenSystemEnabling);
-    auto playerDataModel = Object::FindObjectOfType<PlayerDataModel *>();
-    auto playerData = playerDataModel->playerData;
-    bool customColours = playerData->colorSchemesSettings->overrideDefaultColors;
-    
-    if (!customColours)
-    {
-        playerData->colorSchemesSettings->overrideDefaultColors = true;
-    }
-}
-
 MAKE_AUTO_HOOK_MATCH(StandardLevelDetailViewController_DidActivate, &StandardLevelDetailViewController::DidActivate, void, StandardLevelDetailViewController *self, bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling)
 {
     StandardLevelDetailViewController_DidActivate(self, firstActivation, addedToHierarchy, firstActivation);
