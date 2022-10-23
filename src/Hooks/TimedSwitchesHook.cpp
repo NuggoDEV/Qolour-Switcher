e1faa62f5f9f92ca832dd17bb2a8913c58465d78
Nuggo <NuggoDEV@gmail.com>
2022-10-23 21:31:41 +1030

    Something happened

---
+++ src/Hooks/TimedSwitchesHook.cpp
@@ -0,0 +1,54 @@
+#include "main.hpp"
+#include "QolourSwitcherHooks.hpp"
+#include "QolourSwitcherConfig.hpp"
+
+#include "GlobalNamespace/AudioTimeSyncController.hpp"
+
+#include "GlobalNamespace/PlayerData.hpp"
+#include "GlobalNamespace/PlayerDataModel.hpp"
+#include "GlobalNamespace/ColorScheme.hpp"
+#include "GlobalNamespace/ColorSchemesSettings.hpp"
+using namespace GlobalNamespace;
+
+#include "UnityEngine/Object.hpp"
+#include "UnityEngine/Color.hpp"
+using namespace UnityEngine;
+
+#include "chroma/shared/NoteAPI.hpp"
+#include "chroma/shared/BombAPI.hpp"
+#include "chroma/shared/ObstacleAPI.hpp"
+using namespace Chroma;
+
+MAKE_AUTO_HOOK_MATCH(AudioTimeSyncController_Update, &AudioTimeSyncController::Update, void, AudioTimeSyncController *self)
+{
+    AudioTimeSyncController_Update(self);
+    int timeData = self->songTime;
+
+    auto playerDataModel = Object::FindObjectOfType<PlayerDataModel *>();
+    auto playerData = playerDataModel->playerData;
+    auto colourScheme = playerData->colorSchemesSettings->GetColorSchemeForId(playerData->colorSchemesSettings->selectedColorSchemeId);
+
+    auto colourA = colourScheme->saberAColor;
+    auto colourB = colourScheme->saberBColor;
+
+    if (timeData == 10)
+    {
+        NoteAPI::setGlobalNoteColorSafe(Color(0.1, 0.1, 0.1, 0.5), Color(0.5, 0.5, 0.5, 0.1));
+    } else if (timeData == 15)
+    {
+        NoteAPI::setGlobalNoteColorSafe(colourA, colourB);
+    }
+    
+}
+MAKE_AUTO_HOOK_MATCH(a, &AudioTimeSyncController::Awake, void, AudioTimeSyncController *self)
+{
+    a(self);
+
+    auto SongTimeModel = Object::FindObjectOfType<AudioTimeSyncController *>();
+    float SongTime = SongTimeModel->songTime;
+
+    if (SongTime == 10.0f)
+    {
+        NoteAPI::setGlobalNoteColorSafe(Color(1, 0, 0, 1), Color(0, 1, 0, 1));
+    }
+}
