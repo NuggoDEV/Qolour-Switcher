0b28fbda85c95adba389cf49c3e8bb212468e585
Nuggo <NuggoDEV@gmail.com>
2022-10-26 17:42:02 +1030

    Removed one of many upcoming things from here

--- include/ModConfig.hpp
+++ include/ModConfig.hpp
@@ -8,7 +8,6 @@ using namespace UnityEngine;
 DECLARE_CONFIG(ModConfig, 
     CONFIG_VALUE(DidUserCrash, bool, "If the user crashes before results screen", true);
     CONFIG_VALUE(ColoursChanged, bool, "When a Colour Gets Changed", false);
-    CONFIG_VALUE(BombColour, Color, "Bomb Colour Value", Color(0.1, 0.1, 0.1, 1));
 
     CONFIG_VALUE(LeftStart, Color, "Left Saber Level Start Value", Color(1, 0, 0, 1));
     CONFIG_VALUE(RightStart, Color, "Right Saber Level Start Value", Color(0, 0, 1, 1));
@@ -40,7 +39,6 @@ DECLARE_CONFIG(ModConfig,
     CONFIG_INIT_FUNCTION(
         CONFIG_INIT_VALUE(DidUserCrash);
         CONFIG_INIT_VALUE(ColoursChanged);
-        CONFIG_INIT_VALUE(BombColour);
 
         CONFIG_INIT_VALUE(LeftStart);
         CONFIG_INIT_VALUE(RightStart);
