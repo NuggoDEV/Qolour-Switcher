8d66bb2f5b4fdb0e953f7ec1d8417ddbd32f11d2
NuggoDEV <NuggoDEV@gmail.com>
2022-12-24 14:17:41 +1030

    Formatting works correct :O Gotta make config values work now

--- include/UI/ViewControllers/QolourSwitcherViewController.hpp
+++ include/UI/ViewControllers/QolourSwitcherViewController.hpp
@@ -2,10 +2,23 @@
 
 #include "HMUI/ViewController.hpp"
 #include "custom-types/shared/macros.hpp"
+#include "custom-types/shared/coroutine.hpp"
+
+#include "bsml/shared/macros.hpp"
+#include "bsml/shared/BSML/Components/Settings/SliderSetting.hpp"
+using namespace BSML;
+
+#include "UnityEngine/Color.hpp"
+using namespace UnityEngine;
 
 #define GET_FIND_METHOD(mPtr) il2cpp_utils::il2cpp_type_check::MetadataGetter<mPtr>::get()
 
 DECLARE_CLASS_CODEGEN(QolourSwitcher::UI::ViewControllers, QolourSwitcherViewController, HMUI::ViewController,
-
     DECLARE_OVERRIDE_METHOD(void, DidActivate, GET_FIND_METHOD(&HMUI::ViewController::DidActivate), bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling);
+
+    DECLARE_INSTANCE_FIELD(SliderSetting*, TimeSlider1);
+    DECLARE_INSTANCE_FIELD(SliderSetting*, TimeSlider2);
+    DECLARE_INSTANCE_FIELD(SliderSetting*, TimeSlider3);
+    DECLARE_INSTANCE_FIELD(SliderSetting*, TimeSlider4);
+
 )
--- src/UI/ViewControllers/QolourSwitcherViewController.cpp
+++ src/UI/ViewControllers/QolourSwitcherViewController.cpp
@@ -1,5 +1,7 @@
 #include "UI/ViewControllers/QolourSwitcherViewController.hpp"
 using namespace QolourSwitcher::UI::ViewControllers;
+using namespace QolourSwitcher::UI;
+
 
 #include "ModConfig.hpp"
 #include "bsml/shared/BSML.hpp"
@@ -7,11 +9,29 @@ using namespace QolourSwitcher::UI::ViewControllers;
 
 DEFINE_TYPE(QolourSwitcher::UI::ViewControllers, QolourSwitcherViewController);
 
+
 void QolourSwitcherViewController::DidActivate(bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling)
 {
     if (!firstActivation)
         return;
 
     BSML::parse_and_construct(IncludedAssets::settings_bsml, this->get_transform(), this);
+
+    std::function<std::string(float)> TimeFormatFunction = [](float value)
+    {
+        int minutes = value / 60;
+        int seconds = value - minutes * 60;
+        std::string minuteStr = std::to_string(minutes);
+        std::string secondStr = std::to_string(seconds);
+        if (minutes == 0)
+            return secondStr + "s";
+        else if (minutes > 0 && seconds == 0)
+            return minuteStr + "m";
+        else
+            return minuteStr + "m : " + secondStr + "s"; 
+    };
+    TimeSlider1->formatter = TimeFormatFunction;
+    TimeSlider2->formatter = TimeFormatFunction;
+    TimeSlider3->formatter = TimeFormatFunction;
+    TimeSlider4->formatter = TimeFormatFunction;
 }
-//oid MainViewCon
